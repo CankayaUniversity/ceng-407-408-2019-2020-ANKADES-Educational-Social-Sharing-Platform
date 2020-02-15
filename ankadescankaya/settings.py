dafbbe86b0cca63ffa68cfc9e2a04c870f2d4f96
bugraahmetcaglar <bugraahmetcaglar@gmail.com>
2020-02-15 21:14:36 +0300

    Added applications to INSTALLED_APPS in setting.py file

--- ankadescankaya/settings.py
+++ ankadescankaya/settings.py
@@ -31,6 +31,12 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'account',
+    'adminpanel',
+    'article',
+    'course',
+    'exam',
+    'question',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
