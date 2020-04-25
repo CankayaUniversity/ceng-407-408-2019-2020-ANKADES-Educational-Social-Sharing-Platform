4160387491682f374f56b3f66685aba03549340f
Bugra Ahmet Caglar <bugraahmetcaglar@gmail.com>
2020-04-26 02:26:36 +0300

    added support application and postNumber

---
+++ support/models.py
@@ -0,0 +1,29 @@
+import uuid
+
+from django.db import models
+
+from account.models import Account
+from ankadescankaya.storage_backends import SupportMediaStorage
+
+
+class Report(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=254, unique=True)
+    displayMessage = models.CharField(max_length=255, blank=True, null=True)
+    supportNumber = models.CharField(unique=True, null=False, blank=False, max_length=32)
+    post = models.CharField(null=False, blank=False, max_length=32, verbose_name="Application Post Number")
+    description = models.CharField(max_length=254)
+    isActive = models.BooleanField(default=True)
+    isRead = models.BooleanField(default=True)
+    isSolved = models.BooleanField(default=True)
+    owner = models.ForeignKey(Account, on_delete=models.CASCADE, null=True, blank=True)
+    createdDate = models.DateTimeField(auto_now_add=True)
+    updatedDate = models.DateTimeField(auto_now_add=True)
+    media = models.FileField(null=True, blank=True, storage=SupportMediaStorage())
+
+    def __str__(self):
+        return self.supportNumber
+
+    class Meta:
+        db_table = "Report"
+        ordering = ['-createdDate']
