cdf9f8f36231d000af0852f795f1bd378df54a12
Bugra Ahmet Caglar <bugraahmetcaglar@gmail.com>
2020-05-02 13:05:53 +0300

    fixed bugs

--- exam/urls.py
+++ exam/urls.py
@@ -7,10 +7,10 @@ from exam.views import schools, views
 
 urlpatterns = [
     # url(r'^(?P<slug>[\w-]+)/bolumler/$', views.all_departments, name="all_departments"),
-    # path('okullar/', schools.all_schools, name="all_schools"),
+    path('okullar/', schools.all_schools, name="all_schools"),
 
-    # path('<slug:slug>/bolumler', views.all_departments, name="all_departments"),
-    # path('bolum-ekle/', views.add_department, name="add_department"),
+    path('<slug:slug>/bolumler', schools.all_departments, name="all_departments"),
+    path('bolum-ekle/', schools.add_department, name="add_department"),
     # url(r'^bolum-ekle/(?P<slug>[\w-]+)/$', views.add_department, name="add_department"),
 ]
 
--- exam/views/schools.py
+++ exam/views/schools.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+import datetime
+
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from django.utils.crypto import get_random_string
 
 from ankadescankaya.views import current_user_group, Categories
-from exam.models import School
+from exam.models import School, Department
 
 
 def all_schools(request):
@@ -11,7 +15,7 @@ def all_schools(request):
     """
     userGroup = current_user_group(request, request.user)
     categories = Categories.all_categories()
-    schools = School.objects.filter(isActive=True).order_by('title')
+    schools = Department.objects.filter(isActive=True, schoolId_id__isnull=False).order_by('title')
     context = {
         "userGroup": userGroup,
         "articleCategories": categories[0],
@@ -26,3 +30,59 @@ def all_schools(request):
         "schools": schools,
     }
     return render(request, "ankades/exam/all-schools.html", context)
+
+
+def all_departments(request, slug):
+    """
+    :param request:
+    :return:
+    """
+    userGroup = current_user_group(request, request.user)
+    categories = Categories.all_categories()
+    try:
+        departments = Department.objects.get(schoolId=slug, isActive=True)
+    except:
+        messages.error(request, "Okula ait bölümler bulunamadı.")
+        return redirect("all_schools")
+    context = {
+        "userGroup": userGroup,
+        "articleCategories": categories[0],
+        "articleSubCategories": categories[1],
+        "articleLowerCategories": categories[2],
+        "questionCategories": categories[3],
+        "questionSubCategories": categories[4],
+        "questionLowerCategories": categories[5],
+        "courseCategories": categories[6],
+        "courseSubCategories": categories[7],
+        "courseLowerCategories": categories[8],
+        "departments": departments,
+    }
+    return render(request, "ankades/exam/all-schools.html", context)
+
+
+def add_department(request):
+    """
+    :param request:
+    :return:
+    """
+    userGroup = current_user_group(request, request.user)
+    schools = School.objects.filter(isActive=True).order_by('slug')
+    context = {
+        "userGroup": userGroup,
+        "schools": schools,
+    }
+    if request.method == "POST":
+        instance = Department()
+        schoolId = request.POST["schoolId"]
+        title = request.POST.get("title")
+        instance.title = title
+        instance.isActive = True
+        instance.creator = request.user
+        instance.createdDate = datetime.datetime.now()
+        instance.updatedDate = datetime.datetime.now()
+        instance.schoolId_id = schoolId
+        instance.departmentCode = get_random_string(length=32)
+        instance.save()
+        messages.success(request, "Bölüm başarıyla eklendi.")
+        return redirect("admin_add_department")
+    return render(request, "adminpanel/exam/add-department.html", context)
